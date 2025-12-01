2ab67ddf4c6538dd554e3ec8c759f657b9c594d8
BicEv <bickbovev@gmail.com>
2025-12-01 19:07:42 +0300

    Project structure

---
+++ src/main/java/ru/bicev/finance_analytics/FinanceAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package ru.bicev.finance_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bicev/finance_analytics/FinanceAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bicev.finance_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
