6f6638c5b72be2ee0e0ecb0bbd8d7b87c233a29b
BicEv <bickbovev@gmail.com>
2025-12-13 17:37:24 +0300

    GeneralExceptionHandler

---
+++ src/main/java/ru/bicev/finance_analytics/exception/GeneralExceptionHandler.java
@@ -0,0 +1,58 @@
+package ru.bicev.finance_analytics.exception;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.validation.ConstraintViolationException;
+import ru.bicev.finance_analytics.dto.ErrorResponse;
+import ru.bicev.finance_analytics.dto.FieldErrorDto;
+import ru.bicev.finance_analytics.dto.ValidationErrorResponse;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(404, ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(400, ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(400, ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+        List<FieldErrorDto> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> new FieldErrorDto(e.getField(), e.getDefaultMessage()))
+                .toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ValidationErrorResponse(errors));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ValidationErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        List<FieldErrorDto> errors = ex.getConstraintViolations()
+                .stream()
+                .map(e -> new FieldErrorDto(e.getPropertyPath().toString(), e.getMessage())).toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ValidationErrorResponse(errors));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(500, "Internal Server Error"));
+    }
+
+}
