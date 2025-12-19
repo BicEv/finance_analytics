539e804e790ec83317768b7b5b3159cd6b8343b1
BicEv <bickbovev@gmail.com>
2025-12-19 20:09:57 +0300

    Moved param to a new line (more readable)

--- src/main/java/ru/bicev/finance_analytics/dto/CategoryRequest.java
+++ src/main/java/ru/bicev/finance_analytics/dto/CategoryRequest.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import ru.bicev.finance_analytics.util.CategoryType;
 
-public record CategoryRequest(@NotEmpty(message = "Category must have a name") String name,
+public record CategoryRequest(
+        @NotEmpty(message = "Category must have a name") String name,
         @NotNull(message = "Category type cannot be null") CategoryType type,
         String color) {
 
