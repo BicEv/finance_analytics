dcb1fb9b74d2262c8b61ca4c24664a8b9857e8f0
BicEv <bickbovev@gmail.com>
2025-12-19 20:10:14 +0300

    Completed CategoryRestControllerIT

--- src/test/java/ru/bicev/finance_analytics/integration/CategoryRestControllerIT.java
+++ src/test/java/ru/bicev/finance_analytics/integration/CategoryRestControllerIT.java
@@ -9,9 +9,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import ru.bicev.finance_analytics.entity.Category;
 import ru.bicev.finance_analytics.entity.User;
+import ru.bicev.finance_analytics.repo.CategoryRepository;
 import ru.bicev.finance_analytics.repo.UserRepository;
 import ru.bicev.finance_analytics.security.CustomUserPrincipal;
+import ru.bicev.finance_analytics.util.CategoryType;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -20,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.UUID;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,7 +36,26 @@ public class CategoryRestControllerIT {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
     private User user;
+    private Category cat1;
+    private Category cat2;
+    private String request = """
+                                    {
+                                      "name": "Test category",
+                                      "type": "EXPENSE",
+                                      "color": "#ff0000"
+                                    }
+            """;
+    private String invalidRequest = """
+                                    {
+                                      "name": "",
+                                      "type": "",
+                                      "color": ""
+                                    }
+            """;
 
     @BeforeEach
     void setUp() {
@@ -46,25 +69,141 @@ public class CategoryRestControllerIT {
                         .createdAt(LocalDateTime.now())
                         .lastLoginAt(LocalDateTime.now())
                         .build());
+        cat1 = categoryRepository.save(Category.builder()
+                .name("Food")
+                .createdAt(LocalDateTime.now())
+                .type(CategoryType.EXPENSE)
+                .user(user)
+                .color("#ff0000")
+                .build());
+
+        cat2 = categoryRepository.save(Category.builder()
+                .name("Salary")
+                .createdAt(LocalDateTime.now())
+                .type(CategoryType.INCOME)
+                .user(user)
+                .color("#000000ff")
+                .build());
     }
 
+    // ---------------------
+    // createCategory()
+    // ---------------------
     @Test
     void createCategory_success() throws Exception {
         mockMvc.perform(
                 post("/api/categories")
                         .with(oauth2Login().oauth2User(principal()))
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("""
-                                    {
-                                      "name": "Test category",
-                                      "type": "EXPENSE",
-                                      "color": "#ff0000"
-                                    }
-                                """))
+                        .content(request))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.name").value("Test category"))
                 .andExpect(jsonPath("$.type").value("EXPENSE"));
+    }
+
+    @Test
+    void createCategory_invalidData() throws Exception {
+        mockMvc.perform(
+                post("/api/categories")
+                        .with(oauth2Login().oauth2User(principal()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidRequest))
+                .andExpect(status().isBadRequest());
+
+    }
+
+    // ---------------------
+    // getCategories()
+    // ---------------------
+    @Test
+    void getCategories_success() throws Exception {
+        mockMvc.perform(get("/api/categories")
+                .with(oauth2Login().oauth2User(principal()))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value("Food"))
+                .andExpect(jsonPath("$[1].name").value("Salary"));
+    }
+
+    @Test
+    void getCategories_successWithParam() throws Exception {
+        mockMvc.perform(get("/api/categories")
+                .param("type", "EXPENSE")
+                .with(oauth2Login().oauth2User(principal()))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].name").value("Food"));
+
+    }
+
+    // ---------------------
+    // getCategoryById()
+    // ---------------------
+    @Test
+    void getCategoryById_success() throws Exception {
+        mockMvc.perform(get("/api/categories/" + cat1.getId().toString())
+                .with(oauth2Login().oauth2User(principal())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Food"))
+                .andExpect(jsonPath("$.type").value("EXPENSE"));
+    }
+
+    @Test
+    void getCategoryById_notFound() throws Exception {
+        mockMvc.perform(get("/api/categories/" + UUID.randomUUID().toString())
+                .with(oauth2Login().oauth2User(principal())))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code").value("404"));
+    }
+
+    // ---------------------
+    // updateCategory()
+    // ---------------------
+    @Test
+    void updateCategory_success() throws Exception {
+        mockMvc.perform(put("/api/categories/" + cat1.getId().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request)
+                .with(oauth2Login().oauth2User(principal())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Test category"))
+                .andExpect(jsonPath("$.type").value("EXPENSE"));
+    }
+
+    @Test
+    void updateCategory_notFound() throws Exception {
+        mockMvc.perform(put("/api/categories/" + UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request)
+                .with(oauth2Login().oauth2User(principal())))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code").value("404"));
+
+    }
+
+    @Test
+    void updateCategory_invalidData() throws Exception {
+        mockMvc.perform(put("/api/categories/" + cat2.getId().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(invalidRequest)
+                .with(oauth2Login().oauth2User(principal())))
+                .andExpect(status().isBadRequest());
+    }
+
+    // ---------------------
+    // updateCategory()
+    // ---------------------
+    @Test
+    void deleteCategory_success() throws Exception {
+        mockMvc.perform(delete("/api/categories/" + cat1.getId().toString())
+                .with(oauth2Login().oauth2User(principal())))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteCategory_notFound() throws Exception {
+        mockMvc.perform(delete("/api/categories/" + UUID.randomUUID().toString())
+                .with(oauth2Login().oauth2User(principal())))
+                .andExpect(status().isNotFound());
     }
 
     private CustomUserPrincipal principal() {
@@ -72,7 +211,5 @@ public class CategoryRestControllerIT {
                 "sub", "test-sub",
                 "email", "test@email.com"));
     }
-
-    
 
 }
