7131574445f07922a7a4ebae1818e5054f88e0b6
BicEv <bickbovev@gmail.com>
2025-12-23 19:56:04 +0300

    moved parameters to a separete lines

--- src/main/java/ru/bicev/finance_analytics/dto/RecurringTransactionRequest.java
+++ src/main/java/ru/bicev/finance_analytics/dto/RecurringTransactionRequest.java
@@ -8,10 +8,12 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import ru.bicev.finance_analytics.util.Frequency;
 
-public record RecurringTransactionRequest(@NotNull(message = "Category id cannot be null") UUID categoryId,
+public record RecurringTransactionRequest(
+                @NotNull(message = "Category id cannot be null") UUID categoryId,
                 @NotNull(message = "Amount cannot be null") @DecimalMin(value = "0.00", inclusive = false, message = "Amount must be bigger than zero") BigDecimal amount,
                 @NotNull(message = "Frequency cannot be null") Frequency frequency,
-                @NotNull(message = "Next execution day cannot be null") LocalDate nextExecutionDate, String description,
-                boolean isActive) {
+                @NotNull(message = "Next execution day cannot be null") LocalDate nextExecutionDate, 
+                String description,
+                Boolean isActive) {
 
 }
