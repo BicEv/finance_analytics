4bc55449710cbc19a5dc42f51051d26f8473b679
BicEv <bickbovev@gmail.com>
2025-12-27 17:55:30 +0300

    JwtAuthenticationFilter

---
+++ src/main/java/ru/bicev/finance_analytics/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package ru.bicev.finance_analytics.security.jwt;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.bicev.finance_analytics.repo.UserRepository;
+import ru.bicev.finance_analytics.security.CustomUserPrincipal;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = extractTokenFromCookie(request);
+
+        if (token != null && jwtService.isValid(token)) {
+            Long userId = jwtService.getUserId(token);
+
+            userRepository.findById(userId).ifPresent(user -> {
+                CustomUserPrincipal principal = new CustomUserPrincipal(user, Map.of());
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        principal,
+                        null,
+                        principal.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            });
+
+        }
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String extractTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+
+        for (Cookie cookie : request.getCookies()) {
+            if ("ACCESS_TOKEN".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+}
