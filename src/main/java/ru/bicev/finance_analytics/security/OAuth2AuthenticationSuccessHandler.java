9902ca03645e06ba2831becfb9c758246e43bd0e
BicEv <bickbovev@gmail.com>
2026-01-13 20:42:06 +0300

    Fixed bug with ClassCastException in SuccessHandler

--- src/main/java/ru/bicev/finance_analytics/security/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/ru/bicev/finance_analytics/security/OAuth2AuthenticationSuccessHandler.java
@@ -7,18 +7,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ru.bicev.finance_analytics.entity.User;
 import ru.bicev.finance_analytics.security.jwt.JwtService;
+import ru.bicev.finance_analytics.service.UserService;
 
 @Component
 public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtService jwtService;
+    private final UserService userService;
 
     @Value("${app.security.secure-cookie}")
     private boolean isProd;
@@ -27,14 +32,27 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
     private String redirectUrl;
 
     public OAuth2AuthenticationSuccessHandler(
-            JwtService jwtService) {
+            JwtService jwtService, UserService userService) {
         this.jwtService = jwtService;
+        this.userService = userService;
     }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-        CustomUserPrincipal principal = (CustomUserPrincipal) authentication.getPrincipal();
+        Object principalObj = authentication.getPrincipal();
+
+        CustomUserPrincipal principal;
+        if (principalObj instanceof CustomUserPrincipal) {
+            principal = (CustomUserPrincipal) principalObj;
+        } else if (principalObj instanceof OAuth2User oAuth2User) {
+            User user = userService.getOrCreateOAuthUser(
+                    ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId(),
+                    oAuth2User);
+            principal = new CustomUserPrincipal(user, oAuth2User.getAttributes());
+        } else {
+            throw new IllegalStateException("Unsupported principal type: " + principalObj.getClass());
+        }
 
         String jwt = jwtService.generateToken(principal.getUser().getId());
 
