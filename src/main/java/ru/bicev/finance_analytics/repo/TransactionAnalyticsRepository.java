3cec975cbe8c6a24d81d61cbac12162c151f1599
BicEv <bickbovev@gmail.com>
2026-03-16 19:00:46 +0300

    Reodered mthods (JPQL first, then natives), added missing join and changed queries for new table names

--- src/main/java/ru/bicev/finance_analytics/repo/TransactionAnalyticsRepository.java
+++ src/main/java/ru/bicev/finance_analytics/repo/TransactionAnalyticsRepository.java
@@ -17,64 +17,65 @@ import ru.bicev.finance_analytics.repo.projection.TopCategoryProjection;
 @org.springframework.stereotype.Repository
 public interface TransactionAnalyticsRepository extends Repository<Transaction, UUID> {
 
-    @Query("""
-            SELECT new ru.bicev.finance_analytics.dto.CategoryExpenseDto(
-            t.category.name,
-            SUM(t.amount)
-            )
-            FROM Transaction t
-            WHERE t.user.id = :userId AND t.date BETWEEN :start AND :end
-            GROUP BY t.category.name
-            ORDER BY SUM(t.amount) DESC
-            """)
-    List<CategoryExpenseDto> getExpensesByCategory(Long userId, LocalDate start, LocalDate end);
+        @Query("""
+                        SELECT new ru.bicev.finance_analytics.dto.CategoryExpenseDto(
+                        t.category.name,
+                        SUM(t.amount)
+                        )
+                        FROM Transaction t
+                        WHERE t.user.id = :userId AND t.date BETWEEN :start AND :end
+                        GROUP BY t.category.name
+                        ORDER BY SUM(t.amount) DESC
+                        """)
+        List<CategoryExpenseDto> getExpensesByCategory(Long userId, LocalDate start, LocalDate end);
 
-    @Query(value = """
-            SELECT
-            c.name AS categoryName,
-            SUM(t.amount) AS totalAmount
-            FROM transaction t
-            JOIN сategory c ON t.category_id = c.id
-            WHERE t.user_id = :userId
-            AND t.date BETWEEN :start AND :end
-            GROUP BY c.name
-            ORDER BY totalAmount DESC
-            LIMIT :limit
-            """, nativeQuery = true)
-    List<TopCategoryProjection> getTopCategories(Long userId, LocalDate start, LocalDate end, int limit);
+        @Query("""
+                        SELECT new ru.bicev.finance_analytics.dto.DailyExpenseDto(
+                        t.date,
+                        SUM(t.amount)
+                        )
+                        FROM Transaction t
+                        WHERE t.user.id = :userId AND t.date BETWEEN :start AND :end
+                        GROUP BY t.date
+                        ORDER BY t.date ASC
+                        """)
+        List<DailyExpenseDto> getDailyExpenses(Long userId, LocalDate start, LocalDate end);
 
-    @Query("""
-            SELECT new ru.bicev.finance_analytics.dto.DailyExpenseDto(
-            t.date,
-            SUM(t.amount)
-            )
-            FROM Transaction t
-            WHERE t.user.id = :userId AND t.date BETWEEN :start AND :end
-            GROUP BY t.date
-            ORDER BY t.date ASC
-            """)
-    List<DailyExpenseDto> getDailyExpenses(Long userId, LocalDate start, LocalDate end);
+        @Query(value = """
+                        SELECT
+                        c.name AS categoryName,
+                        SUM(t.amount) AS totalAmount
+                        FROM transactions t
+                        JOIN category c ON t.category_id = c.id
+                        WHERE t.user_id = :userId
+                        AND t.date BETWEEN :start AND :end
+                        GROUP BY c.name
+                        ORDER BY totalAmount DESC
+                        LIMIT :limit
+                        """, nativeQuery = true)
+        List<TopCategoryProjection> getTopCategories(Long userId, LocalDate start, LocalDate end, int limit);
 
-    @Query(value = """
-            SELECT
-            DATE_TRUNC('month', t.date) AS month,
-            SUM(t.amount) AS totalAmount
-            FROM transaction t
-            WHERE t.user_id = :userId
-            AND t.date BETWEEN :start AND :end
-            GROUP BY month
-            ORDER BY month
-            """, nativeQuery = true)
-    List<MonthlyExpenseProjection> getMonthlyExpenses(Long userId, LocalDate start, LocalDate end);
+        @Query(value = """
+                        SELECT
+                        DATE_TRUNC('month', t.date) AS month,
+                        SUM(t.amount) AS totalAmount
+                        FROM transactions t
+                        WHERE t.user_id = :userId
+                        AND t.date BETWEEN :start AND :end
+                        GROUP BY month
+                        ORDER BY month
+                        """, nativeQuery = true)
+        List<MonthlyExpenseProjection> getMonthlyExpenses(Long userId, LocalDate start, LocalDate end);
 
-    @Query(value = """
-            SELECT
-            SUM(CASE WHEN t.type = 'INCOME' then t.amount ELSE 0 END)::numeric AS income,
-            SUM(CASE WHEN t.type = 'EXPENSE' then t.amount ELSE 0 END)::numeric AS expense
-            FROM transaction t
-            WHERE t.user_id = :userId
-            AND t.date BETWEEN :start AND :end
-            """, nativeQuery = true)
-    SummaryProjection getSummary(Long userId, LocalDate start, LocalDate end);
+        @Query(value = """
+                        SELECT
+                        SUM(CASE WHEN c.type = 'INCOME' then t.amount ELSE 0 END)::numeric AS income,
+                        SUM(CASE WHEN c.type = 'EXPENSE' then t.amount ELSE 0 END)::numeric AS expense
+                        FROM transactions t
+                        JOIN categories c ON t.category_id = c.id
+                        WHERE t.user_id = :userId
+                        AND t.date BETWEEN :start AND :end
+                        """, nativeQuery = true)
+        SummaryProjection getSummary(Long userId, LocalDate start, LocalDate end);
 
 }
