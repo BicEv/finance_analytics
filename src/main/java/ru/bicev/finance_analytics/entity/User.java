8596103405bae21197b5cc792e304821a66fe52f
BicEv <bickbovev@gmail.com>
2026-03-16 17:40:46 +0300

    Added id generator for Long userId in User entity

--- src/main/java/ru/bicev/finance_analytics/entity/User.java
+++ src/main/java/ru/bicev/finance_analytics/entity/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,7 +22,12 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_gen")
+    @SequenceGenerator(
+        name = "seq_gen",
+        sequenceName = "transaction_id_seq",
+        allocationSize = 50
+    )
     private Long id;
 
     private String provider;
