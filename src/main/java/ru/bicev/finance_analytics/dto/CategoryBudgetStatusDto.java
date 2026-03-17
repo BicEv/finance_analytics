d1a4f06b9219002ef1449f0654ae73dfb57fe908
BicEv <bickbovev@gmail.com>
2026-03-17 19:12:49 +0300

    Added method helper to create DTO with percent

--- src/main/java/ru/bicev/finance_analytics/dto/CategoryBudgetStatusDto.java
+++ src/main/java/ru/bicev/finance_analytics/dto/CategoryBudgetStatusDto.java
@@ -2,11 +2,14 @@ package ru.bicev.finance_analytics.dto;
 
 import java.math.BigDecimal;
 
-
 public record CategoryBudgetStatusDto(
-        String category,
-        BigDecimal limit,
-        BigDecimal spent,
-        BigDecimal percentUsed) {
+                String category,
+                BigDecimal limit,
+                BigDecimal spent,
+                BigDecimal percentUsed) {
+
+        public CategoryBudgetStatusDto withPercent(BigDecimal percent) {
+                return new CategoryBudgetStatusDto(category, limit, spent, percent);
+        }
 
 }
