2be2735c9b19f3679fabb5d251fb9e8b482adcfa
BicEv <bickbovev@gmail.com>
2026-03-18 18:01:41 +0300

    Added jooqRepo and removed all other repos in analyticsService

--- src/main/java/ru/bicev/finance_analytics/service/AnalyticsService.java
+++ src/main/java/ru/bicev/finance_analytics/service/AnalyticsService.java
@@ -2,12 +2,9 @@ package ru.bicev.finance_analytics.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.time.LocalDate;
 import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,14 +19,8 @@ import ru.bicev.finance_analytics.dto.MonthlyExpenseDto;
 import ru.bicev.finance_analytics.dto.RecurringForecastDto;
 import ru.bicev.finance_analytics.dto.SummaryDto;
 import ru.bicev.finance_analytics.dto.TopCategoryDto;
-import ru.bicev.finance_analytics.entity.Budget;
-import ru.bicev.finance_analytics.entity.RecurringTransaction;
-import ru.bicev.finance_analytics.entity.Transaction;
 import ru.bicev.finance_analytics.exception.NotFoundException;
-import ru.bicev.finance_analytics.repo.BudgetRepository;
-import ru.bicev.finance_analytics.repo.RecurringTransactionRepository;
-import ru.bicev.finance_analytics.repo.TransactionRepository;
-import ru.bicev.finance_analytics.util.CategoryType;
+import ru.bicev.finance_analytics.repo.JooqAnalyticsRepository;
 
 /**
  * Сервис, выполняющий аналитику расходов для текущего пользователя
@@ -38,22 +29,14 @@ import ru.bicev.finance_analytics.util.CategoryType;
 @Transactional(readOnly = true)
 public class AnalyticsService {
 
-        private final TransactionRepository transactionRepository;
-        private final RecurringTransactionRepository recurringTransactionRepository;
-        private final BudgetRepository budgetRepository;
+        private final JooqAnalyticsRepository jooqRepo;
         private final UserService userService;
 
         private static final Logger logger = LoggerFactory.getLogger(AnalyticsService.class);
 
-        private static final BigDecimal ZERO = BigDecimal.ZERO;
-
-        public AnalyticsService(TransactionRepository transactionRepository,
-                        RecurringTransactionRepository recurringTransactionRepository,
-                        BudgetRepository budgetRepository,
+        public AnalyticsService(JooqAnalyticsRepository jooqRepo,
                         UserService userService) {
-                this.transactionRepository = transactionRepository;
-                this.recurringTransactionRepository = recurringTransactionRepository;
-                this.budgetRepository = budgetRepository;
+                this.jooqRepo = jooqRepo;
                 this.userService = userService;
         }
 
@@ -66,21 +49,10 @@ public class AnalyticsService {
          * @return список всех категорий за месяц с суммой расходов на них
          */
         public List<CategoryExpenseDto> getExpensesByCategory(YearMonth month) {
-                List<Transaction> transactions = getExpensesAndMonth(month);
 
                 logger.debug("getExpensesByCategory() for month: {}", month.toString());
-                return transactions.stream()
-                                .collect(Collectors.groupingBy(
-                                                Transaction::getCategory,
-                                                Collectors.reducing(ZERO, Transaction::getAmount,
-                                                                BigDecimal::add)))
-                                .entrySet()
-                                .stream()
-                                .map(e -> new CategoryExpenseDto(
-                                                e.getKey().getName(),
-                                                e.getValue().setScale(2)))
-                                .sorted((e1, e2) -> e2.total().compareTo(e1.total()))
-                                .toList();
+                Long userId = getCurrentUserId();
+                return jooqRepo.getExpensesByCategory(userId, month.atDay(1), month.atEndOfMonth());
 
         }
 
@@ -96,14 +68,12 @@ public class AnalyticsService {
          * @throws IllegalArgumentException если {@code limit} меньше или равен нулю
          */
         public List<TopCategoryDto> getTopCategories(YearMonth month, int limit) {
-
-                var expensesByCategory = getExpensesByCategory(month);
+                if (limit <= 0) {
+                        throw new IllegalArgumentException("Limit can not be less than 1");
+                }
                 logger.debug("getTopCategories() for month: {}", month.toString());
-                return expensesByCategory.stream()
-                                .sorted((e1, e2) -> e2.total().compareTo(e1.total()))
-                                .limit(limit)
-                                .map(cat -> new TopCategoryDto(cat.category(), cat.total().setScale(2)))
-                                .toList();
+                Long userId = getCurrentUserId();
+                return jooqRepo.getTopCategories(userId, month.atDay(1), month.atEndOfMonth(), limit);
         }
 
         /**
@@ -113,20 +83,9 @@ public class AnalyticsService {
          * @return список трат за укзанный месяц по дням
          */
         public List<DailyExpenseDto> getDailyExpenses(YearMonth month) {
-                List<Transaction> transactions = getExpensesAndMonth(month);
-
                 logger.debug("getDailyExpenses() for month: {}", month.toString());
-                return transactions.stream()
-                                .collect(Collectors.groupingBy(
-                                                t -> t.getDate(),
-                                                Collectors.mapping(Transaction::getAmount,
-                                                                Collectors.reducing(ZERO,
-                                                                                BigDecimal::add))))
-                                .entrySet()
-                                .stream()
-                                .map(e -> new DailyExpenseDto(e.getKey(), e.getValue().setScale(2)))
-                                .sorted((e1, e2) -> e1.date().compareTo(e2.date()))
-                                .toList();
+                Long userId = getCurrentUserId();
+                return jooqRepo.getDailyExpenses(userId, month.atDay(1), month.atEndOfMonth());
         }
 
         /**
@@ -142,52 +101,25 @@ public class AnalyticsService {
                         throw new IllegalStateException("Start of date range can not be after end");
 
                 }
-                List<Transaction> transactions = transactionRepository
-                                .findAllByUserIdAndCategory_TypeAndDateBetween(getCurrentUserId(),
-                                                CategoryType.EXPENSE, range.start(), range.end());
                 logger.debug("getMonthlyExpenses() for range from: {}, to: {}", range.start().toString(),
                                 range.end().toString());
-                return transactions.stream()
-                                .collect(Collectors.groupingBy(
-                                                t -> YearMonth.from(t.getDate()),
-                                                Collectors.reducing(ZERO, Transaction::getAmount,
-                                                                BigDecimal::add)))
-                                .entrySet()
-                                .stream()
-                                .map(e -> new MonthlyExpenseDto(
-                                                e.getKey().format(DateTimeFormatter.ofPattern("MM.yyyy")),
-                                                e.getValue().setScale(2)))
-                                .sorted((e1, e2) -> e1.month().compareTo(e2.month()))
-                                .toList();
+                Long userId = getCurrentUserId();
+                return jooqRepo.getMonthlyExpenses(userId, range.start(), range.end());
+
         }
 
         /**
-         * Возвращает поступления, суммы лититов бюджетов, траты и баланс за указанный месяц
+         * Возвращает поступления, суммы лититов бюджетов, траты и баланс за указанный
+         * месяц
          * 
          * @param month месяц, за который рассчитываются транзакции
          * @return дто, в котором укзаны поступления, расходы и баланс за указанный
          *         месяц
          */
         public SummaryDto getSummary(YearMonth month) {
-                List<Transaction> transactions = getTransactionsForMonth(month);
-
-                BigDecimal income = budgetRepository.sumAmountByUserIdAndMonth(getCurrentUserId(), month)
-                                .orElse(ZERO);
-
-                BigDecimal expense = ZERO;
-
-                for (Transaction t : transactions) {
-                        if (t.getCategory().getType() == CategoryType.INCOME) {
-                                income = income.add(t.getAmount());
-                        } else {
-
-                                expense = expense.add(t.getAmount());
-                        }
-                }
-
-                BigDecimal balance = income.subtract(expense);
-
-                return new SummaryDto(income, expense, balance);
+                logger.debug("getSummary() for month: {}", month.toString());
+                Long userId = getCurrentUserId();
+                return jooqRepo.getSummaryForMonth(userId, month.atDay(1), month.atEndOfMonth());
         }
 
         /**
@@ -202,35 +134,15 @@ public class AnalyticsService {
          */
         public CategoryBudgetStatusDto getCategoryBudgetStatus(UUID budgetId) {
                 Long userId = getCurrentUserId();
-                Budget budget = budgetRepository.findByIdAndUserId(budgetId, userId)
-                                .orElseThrow(() -> new NotFoundException("Budget not found"));
-                LocalDate start = budget.getMonth().atDay(1);
-                LocalDate end = budget.getMonth().atEndOfMonth();
-
                 logger.debug("getCategoryBudgetStatus() for budget: {}", budgetId.toString());
 
-                List<Transaction> transactions = transactionRepository.findAllByUserIdAndCategoryIdAndDateBetween(
-                                userId,
-                                budget.getCategory().getId(), start, end);
-
-                BigDecimal spent = transactions.stream()
-                                .map(Transaction::getAmount)
-                                .reduce(ZERO, BigDecimal::add)
+                var dto = jooqRepo.getCategoryBudgetStatus(userId, budgetId).orElseThrow(
+                                () -> new NotFoundException("Budget not found for id: " + budgetId.toString()));
+                BigDecimal percentUsed = dto.spent()
+                                .divide(dto.limit())
+                                .multiply(BigDecimal.valueOf(100))
                                 .setScale(2, RoundingMode.HALF_UP);
-
-                BigDecimal percentUsed = ZERO;
-                if (budget.getAmount() != null && budget.getAmount().compareTo(ZERO) > 0) {
-                        percentUsed = spent
-                                        .divide(budget.getAmount(), 2, RoundingMode.HALF_UP)
-                                        .multiply(BigDecimal.valueOf(100))
-                                        .setScale(2, RoundingMode.HALF_UP);
-                }
-
-                return new CategoryBudgetStatusDto(
-                                budget.getCategory().getName(),
-                                budget.getAmount(),
-                                spent,
-                                percentUsed);
+                return dto.withPercent(percentUsed);
         }
 
         /**
@@ -241,49 +153,9 @@ public class AnalyticsService {
          */
         public List<RecurringForecastDto> getUpcomingRecurringPayments() {
                 Long userId = getCurrentUserId();
-                List<RecurringTransaction> transactions = recurringTransactionRepository
-                                .findAllByUserIdAndIsActiveAndNextExecutionDateGreaterThanEqual(userId, true,
-                                                LocalDate.now());
-
                 logger.debug("getUpcomintRecurringPayments() for user: {}", userId);
 
-                return transactions.stream()
-                                .collect(
-                                                Collectors.groupingBy(
-                                                                t -> t.getNextExecutionDate().format(
-                                                                                DateTimeFormatter.ofPattern("MM.yyyy")),
-                                                                Collectors.mapping(RecurringTransaction::getAmount,
-                                                                                Collectors.reducing(ZERO,
-                                                                                                BigDecimal::add))))
-                                .entrySet()
-                                .stream()
-                                .map(e -> new RecurringForecastDto(e.getKey(), e.getValue().setScale(2)))
-                                .sorted((e1, e2) -> e1.month().compareTo(e2.month()))
-                                .toList();
-        }
-
-        /**
-         * Служебный метод, возвращающий список транзакций за указанный месяц
-         * 
-         * @param month месяц, за который получаются транзакции
-         * @return список транзакций за указанный месяц
-         */
-        private List<Transaction> getTransactionsForMonth(YearMonth month) {
-                Long userId = getCurrentUserId();
-                return transactionRepository.findAllByUserIdAndDateBetween(userId,
-                                month.atDay(1), month.atEndOfMonth());
-        }
-
-        /**
-         * Служебный метод, возвращающий список расходов за указанный месяц
-         * 
-         * @param month месяц, за который получаются транзакции
-         * @return список расходов, за указанный месяц
-         */
-        private List<Transaction> getExpensesAndMonth(YearMonth month) {
-                Long userId = getCurrentUserId();
-                return transactionRepository.findAllByUserIdAndCategory_TypeAndDateBetween(userId,
-                                CategoryType.EXPENSE, month.atDay(1), month.atEndOfMonth());
+                return jooqRepo.getUpcomingPayments(userId);
         }
 
         /**
